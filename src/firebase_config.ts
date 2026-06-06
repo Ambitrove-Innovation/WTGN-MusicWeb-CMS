ea0b02f584a03054f4de3dc5a0428a2781226e79
Meyer.dev <meyerdean.developer@gmail.com>
2026-06-06 17:32:33 +0200

    First commit

---
+++ src/firebase_config.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    "projectId": "salondesk-demo",
+    "appId": "1:1061957281868:web:ef1165d61fea4f4d932ce9",
+    "storageBucket": "salondesk-demo.firebasestorage.app",
+    "apiKey": "AIzaSyBO3RahgR6VIr2iDp6GXtFkJi_UHZ7Y1Uc",
+    "authDomain": "salondesk-demo.firebaseapp.com",
+    "messagingSenderId": "1061957281868",
+    "projectNumber": "1061957281868",
+    "version": "2"
+}
