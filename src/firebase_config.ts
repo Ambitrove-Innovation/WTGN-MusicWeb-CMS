3aa797d4bb3b9d8818d85779fc9ec3fc5e944046
Meyer.dev <meyerdean.developer@gmail.com>
2026-06-06 20:13:53 +0200

    Included firebase config file

--- src/firebase_config.ts
+++ src/firebase_config.ts
@@ -1,10 +1,12 @@
+import { version } from "react";
+
 export const firebaseConfig = {
-    "projectId": "salondesk-demo",
-    "appId": "1:1061957281868:web:ef1165d61fea4f4d932ce9",
-    "storageBucket": "salondesk-demo.firebasestorage.app",
-    "apiKey": "AIzaSyBO3RahgR6VIr2iDp6GXtFkJi_UHZ7Y1Uc",
-    "authDomain": "salondesk-demo.firebaseapp.com",
-    "messagingSenderId": "1061957281868",
-    "projectNumber": "1061957281868",
-    "version": "2"
+    "projectId": import.meta.env.VITE_FIREBASE_PROJECTID,
+    "appId": import.meta.env.VITE_FIREBASE_APPID,
+    "storageBucket": import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
+    "apiKey": import.meta.env.VITE_FIREBASE_APIKEY,
+    "authDomain": import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
+    "messagingSenderId": import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID,
+    "projectNumber": import.meta.env.VITE_FIREBASE_PROJECTNUMBER,
+    "version": import.meta.env.VITE_FIREBASE_VERSION
 }
